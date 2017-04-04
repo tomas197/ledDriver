caab7e732e37515568e2e4a4fa335c5bfea3795d
tomas <tomas_197@hotmail.com>
2017-04-03 21:43:32 -0300

    test

--- src/ledDriver.c
+++ src/ledDriver.c
@@ -5,23 +5,23 @@ void ledDriver_create(uint16_t *address) {
 	*address = 0;
 }
 void ledEncendido_individual(uint16_t *address){
-
+*address = 0;
 }
 void ledApagado_individual(uint16_t *address){
-
+*address = 0;
 }
 void ledApagado(uint16_t *address){
-
+*address = 0;
 }
 void ledEncendido(uint16_t *address){
-
+*address = 0;
 }
 void ledEstatus(uint16_t *address){
-
+*address = 0;
 }
 void ledValoresLimites(uint16_t *address){
-
+*address = 0;
 }
 void ledValoresFueraLimites(uint16_t *address){
-
+*address = 0;
 }
